71e457ca0008c223d7f8afda9e37401cf7eadf21
Marcelo Szalontai <marcelo.szalontai@linx.com.br>
2026-02-10 19:56:14 -0300

    Service layer, component registration

---
+++ src/main/java/com/java/bala/curso/services/UserService.java
@@ -0,0 +1,30 @@
+package com.java.bala.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.java.bala.curso.entities.User;
+import com.java.bala.curso.repositories.UserRepository;
+
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll()
+	{
+		return repository.findAll();
+	}
+	
+	public User findById(Long id)
+	{
+		Optional<User> obj =  repository.findById(id);
+		return obj.get();
+	}
+}
+
