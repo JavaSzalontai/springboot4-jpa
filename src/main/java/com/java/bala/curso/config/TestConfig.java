74549ff978d25e9e1419d1620a4394418fb6209a
Marcelo Szalontai <marcelo.szalontai@linx.com.br>
2026-02-18 19:57:53 -0300

    Payment, one-to-one association

--- src/main/java/com/java/bala/curso/config/TestConfig.java
+++ src/main/java/com/java/bala/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.java.bala.curso.entities.Category;
 import com.java.bala.curso.entities.Order;
 import com.java.bala.curso.entities.OrderItem;
+import com.java.bala.curso.entities.Payment;
 import com.java.bala.curso.entities.Product;
 import com.java.bala.curso.entities.User;
 import com.java.bala.curso.entities.enums.OrderStatus;
@@ -77,6 +78,12 @@ public class TestConfig implements CommandLineRunner {
 
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"),o1);
+	
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
+		
+	
 	}
 	
 	
